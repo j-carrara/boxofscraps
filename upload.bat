1b6baec60d29acefa652cf1086c0e9e0800cde1a
Joseph A Carrara <jac1109@wildcats.unh.edu>
2022-11-14 12:13:11 -0500

    update upload.bat

--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
-scp -r -i "***REMOVED***" src ***REMOVED***:~/boxofscraps
+scp -i "***REMOVED***" src/*.py ***REMOVED***:~/boxofscraps/src
 ssh -i "***REMOVED***" ***REMOVED*** pkill screen
 ssh -i "***REMOVED***" ***REMOVED*** screen -s bot -dm python3.8 ~/boxofscraps/src/bot.py
